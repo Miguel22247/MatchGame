dc6d232ee1dd96ebdb457467439581f0b47f1dd5
Miguel22247 <minipachru@gmail.com>
2021-10-07 14:12:36 -0300

    add basic properties for SignIn and SignUp screens

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -5,5 +5,28 @@
 </template>
 
 <style scoped>
+	@media screen and (min-width:821px){
+    .on-desktop {
+        color: #00A7CC;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
 
+    .on-desktop img {
+        position: absolute;
+        left: 50%;
+        transform: translate(-50%, 0%);
+    }
+    .mobile-landing {
+        display: none;
+    }
+
+    .landing-page {
+        background-color: #292929;
+        height: 100%;
+        width: 100%;
+    }
+}
 </style>
