feb17717b4792becf5f6455823214bee069006ae
Miguel22247 <minipachru@gmail.com>
2021-10-07 12:09:38 -0300

    add GAuth support

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import GAuth from 'vue-google-oauth2'
 
 Vue.config.productionTip = false
 
@@ -12,3 +13,10 @@ new Vue({
   vuetify,
   render: h => h(App)
 }).$mount('#app')
+
+const gauthOption = {
+  clientId: 'CLIENT_ID.apps.googleusercontent.com',
+  scope: 'profile email',
+  prompt: 'select_account'
+}
+Vue.use(GAuth, gauthOption)
