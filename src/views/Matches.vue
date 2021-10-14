017a689afe74b0d04a1b7c27fa608c8623dd3ba8
Miguel22247 <minipachru@gmail.com>
2021-10-14 10:46:54 -0300

    add basic code and Desktop error on Matches

--- src/views/Matches.vue
+++ src/views/Matches.vue
@@ -12,5 +12,48 @@
 </script>
 
 <style scoped>
+/*for browsers*/
+@media screen and (min-width:1025px){
+    .landing-page {
+        background-color: #292929;
+        height: 100%;
+        width: 100%;
+    }
 
+    .mobile-landing {
+        display: none;
+    }
+
+    .on-desktop {
+        color: #00A7CC;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+
+    .on-desktop img {
+        position: absolute;
+        left: 50%;
+        transform: translate(-50%, 0%);
+    }
+}
+
+@media screen and (max-width: 1024px) {
+    .on-desktop {
+        display: none;
+    }
+
+    .landing-page {
+        background-color: #292929;
+        height: 100%;
+        width: 100%;
+    }
+
+	* {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+}
 </style>
