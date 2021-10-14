d321901e0483137d3738da46a4cab5aee125eeb6
Miguel22247 <minipachru@gmail.com>
2021-10-14 08:58:57 -0300

    adding basic of matches

---
+++ src/views/Matches.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="landing-page">
+        <div class="on-desktop">
+            <div class="desktop-table">
+                <h3>Check back soon for the Desktop version!</h3>
+                <img class='no-desktop' src="./../../public/icons/sad.png" alt="Sad">
+            </div>
+        </div>
+	</div>
+</template>
+<script>
+</script>
+
+<style scoped>
+
+</style>
