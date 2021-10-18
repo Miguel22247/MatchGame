3edb1dc3dbc570bfe0ae89a4aba187e7ea6e7e08
Miguel22247 <minipachru@gmail.com>
2021-10-18 15:21:02 -0300

    remove firebase code

--- src/main.js
+++ src/main.js
@@ -3,19 +3,8 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
-import firebase from 'firebase';
 
 Vue.config.productionTip = false
-
-let config = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "hbtn-final-project-mvd.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SEND_ID"
-};
-firebase.initializeApp(config);
 
 new Vue({
   router,
