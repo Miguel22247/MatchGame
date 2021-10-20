80fe350672a9742f99eac041963178339896618c
Miguel Pacheco <minipachru@gmail.com>
2021-10-20 16:42:46 -0300

    add some bug fixes

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -13,7 +13,7 @@
             <div class="login-forms">
                 <div class="return-button">
                     <router-link to="/">
-                        <v-btn color="#00A7CC" small>
+                        <v-btn color="#00A7CC" outlined depressed small>
                             <v-icon color="#ffffff">
                                 mdi-arrow-left
                             </v-icon>
@@ -229,6 +229,5 @@
         text-align: center;
         font-size: 9px;
     }
-
 }
 </style>
