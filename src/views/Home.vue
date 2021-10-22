82876c9b89d5fe33b1954898ebdb3844221c5160
Miguel22247 <minipachru@gmail.com>
2021-10-22 16:32:14 -0300

    fix buttons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,10 @@
 <template>
   <v-container fill-height fluid mt-14>
     <v-row align="center" justify="center">
+  		<h2>Welcome to GameMatch</h2>
+	</v-row>
+    <v-row align="center" justify="space-around">
 		<router-link to="/register"><v-btn outlined color="#00A7CC">Sign Up</v-btn></router-link>
-    </v-row>
-	<v-row align="center" justify="center">
 		<router-link to="/login"><v-btn outlined color="#00A7CC">Sign In</v-btn></router-link>
 	</v-row>
   </v-container>
