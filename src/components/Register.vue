cd80be0cb251327fe92dd57592516b73819db7c6
Miguel22247 <minipachru@gmail.com>
2021-10-25 15:49:04 -0300

    FINALLLLLLLLLLY

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,11 +1,11 @@
 <template>
-  <v-container>
+  <v-container class="container" fluid fill-height>
     <v-layout row>
       <v-flex xs12 sm6 offset-sm3>
-        <v-card>
+        <v-card color="#ffffff" outlined>
           <v-card-text>
             <v-container>
-              <form @submit.prevent="onSignup">
+              <form v-on:submit.prevent="onSubmit">
                 <v-layout row>
                   <v-flex xs12>
                     <v-text-field
@@ -26,11 +26,11 @@
                       outlined
                       rounded
                       color="#00A7CC"
-                      name="password"
-                      label="Password"
-                      id="password"
-                      v-model="password"
-                      type="password"
+                      name="username"
+                      label="Username"
+                      id="username"
+                      v-model="username"
+                      type="text"
                       required></v-text-field>
                   </v-flex>
                 </v-layout>
@@ -40,14 +40,14 @@
                       outlined
                       rounded
                       color="#00A7CC"
-                      name="confirmPassword"
-                      label="Confirm Password"
-                      id="confirmPassword"
-                      v-model="confirmPassword"
+                      name="password"
+                      label="Password"
+                      id="password"
+                      v-model="password"
                       type="password"
-                      :rules="[comparePasswords]"></v-text-field>
+                      required></v-text-field>
                   </v-flex>
-                </v-layout>
+                </v-layout> 
                 <v-layout row>
                   <v-flex xs12>
                     <v-btn outlined rounded color="#00A7CC" type="submit">Sign up</v-btn>
@@ -62,45 +62,46 @@
   </v-container>
 </template>
 
-
-
 <script>
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import axios from 'axios'
+
 
 export default {
-  data () {
+  data: function() {
     return {
+      username: '',
       email: '',
       password: '',
     }
   },
-  computed: {
-      comparePasswords () {
-        return this.password !== this.confirmPassword ? 'Passwords do not match' : ''
-      },
-      user () {
-        return this.$store.getters.user
-      }
-    },
-    watch: {
-      user (value) {
-        if (value !== null && value !== undefined) {
-          this.$router.push('/Register')
-        }
-      }
-    },
+  // async created() {
+  //   try {
+      
+  //     const req = await axios.post(apiurl, this.userData);
+
+  //   } catch (e) {
+  //     console.error(e);
+  //   }
+  // },
+
   methods: {
-    onSignup () {
-      const auth = getAuth();
-      createUserWithEmailAndPassword(auth, this.email, this.password)
-      .then((user) => {
-          this.$router.replace('/home')
-        }).catch((err) => {
-          alert(err.message)
-        });
-      }
+    onSubmit: function(){
+       const datos = { 'username': this.username, 'email': this.email, 'password': this.password };
+       const headers = {
+         "Access-Control-Allow-Origin": "*"
+       };
+       axios.post('http://35.190.147.190:5000/api/user', datos, { headers });
+        // .then((res) => {
+        //     //Perform Success Action
+        //     axios.post(url, )
+        // })
+        // .catch((error) => {
+        //     // error.response.status Check status code
+        // });
+        // .finally(() => {
+        //     //Perform action in always
+        // });
     }
-  }
+  },
+}
 </script>
-
-
