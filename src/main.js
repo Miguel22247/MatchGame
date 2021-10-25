a1fa5fee4efeec0f1d8450b2fe0412fabd836377
Miguel22247 <minipachru@gmail.com>
2021-10-25 15:23:27 -0300

    remove firebase code and add axios import

--- src/main.js
+++ src/main.js
@@ -3,10 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
-import { initializeApp } from 'firebase/app';
-import VeeValidate from 'vee-validate'
+import axios from 'axios'
 
-Vue.use(VeeValidate)
 Vue.config.productionTip = false
 
 new Vue({
