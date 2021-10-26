1e78e405f94d11a68b2dd84080aaff33757996ff
Miguel22247 <minipachru@gmail.com>
2021-10-26 09:42:59 -0300

    add logo

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -3,6 +3,13 @@
       <v-row align="center" justify="center">
           <h2>Welcome to GameMatch</h2>
       </v-row>
+      <v-row align="center" justify="center">
+          <v-img
+            max-height="250"
+            max-width="250"
+            src="../../public/icons/GameMatch.png"
+          ></v-img>
+      </v-row>
       <v-row align="center" justify="space-around">
         <v-btn outlined color="#00A7CC" to="/register">Sign Up</v-btn>
         <v-btn outlined color="#00A7CC" to="/login">Sign In</v-btn>
