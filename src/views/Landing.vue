701b0563062aea49a6a05be91df96ff6ebb4d7bb
Miguel22247 <minipachru@gmail.com>
2021-10-27 11:12:40 -0300

    fix size

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,12 +1,19 @@
 <template>
-  <v-container fill-height fluid mt-14>
-    <v-row align="top" justify="center">
-        <h2>Welcome to GameMatch</h2>
-    </v-row>
-    <v-row align="end" justify="space-around">
+  <v-container class="container" fluid fill-height>
+      <v-row align="center" justify="center">
+          <h2>Welcome to GameMatch</h2>
+      </v-row>
+      <v-row align="center" justify="center">
+          <v-img
+            max-height="250"
+            max-width="250"
+            src="../../public/icons/GameMatch.png"
+          ></v-img>
+      </v-row>
+      <v-row align="center" justify="space-around">
         <v-btn outlined color="#00A7CC" to="/register">Sign Up</v-btn>
         <v-btn outlined color="#00A7CC" to="/login">Sign In</v-btn>
-    </v-row>
+      </v-row>
   </v-container>
 </template>
 
@@ -23,4 +30,9 @@ export default {
   a {
     text-decoration: none;
   }
+
+  .container {
+    height: 100%;
+  }
+
 </style>
