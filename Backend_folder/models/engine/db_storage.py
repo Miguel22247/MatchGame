f204bcc3cecd433daeba7e415b0e92e4ea7d4982
Gabriel Prochnau <2288@holbertonschool.com>
2021-10-28 23:16:02 -0300

    Modified sqlalchemy session

--- Backend_folder/models/engine/db_storage.py
+++ Backend_folder/models/engine/db_storage.py
@@ -58,7 +58,8 @@ class DBStorage:
         and initializes a new session"""
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine,
-                                       expire_on_commit=False)
+                                       expire_on_commit=False,
+                                       autoflush=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
 
