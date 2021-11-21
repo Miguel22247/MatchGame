160a0ff158ec8f5ff2e018bfc233cbccdfdeebd8
Miguel Pacheco <minipachru@gmail.com>
2021-11-21 20:46:03 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
+const back2Top = document.querySelector('#back2Top');
 
+back2Top.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+});
